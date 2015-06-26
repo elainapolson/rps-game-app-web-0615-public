c8ab385df79cd0f35681c142fc9b846be6e1510c
elainapolson <elainapolson@gmail.com>
2015-06-26 14:23:55 -0400

    fixed a bunch of stuff

--- app.rb
+++ app.rb
@@ -1,13 +1,10 @@
 require_relative 'config/environment'
 
+
 class GameApp < Sinatra::Base
   get '/rps/:play' do
 
-    @new_game = RPSGame.new(params[:play].to_sym)
-
-    @play = params[:play]
-
-    @computer_play = @new_game.computer_play
+    @game = RPSGame.new(params["play"].to_sym)
 
     erb :rps_game
   end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,16 +1,19 @@
-require_relative 'play_time_error'
-
-class RPSGame
+require_relative 'play_type_error'
+require 'pry'
+ class RPSGame
 
   attr_reader :play, :computer_play
 
   MOVES = [:rock, :paper, :scissors]
 
   def initialize(play)
-    @play = play
-    if !MOVES.include?(@play)
+    if MOVES.include?(play)
+      @play = play
+    else
       raise PlayTypeError
     end
+
+    set_computer_move
   end
 
   def self.valid_play?(play)
@@ -21,12 +24,20 @@ class RPSGame
     end
   end
 
-  def computer_play
+  def set_computer_move
     @computer_play = MOVES.sample
   end
 
+  def computer_play
+    @computer_play
+  end
+
   def won?
-    if (@computer_play == :scissors && @play == :rock) || (@computer_play == :rock && @play == :paper) || (@computer_play == :paper && @play == :scissors)
+    if self.computer_play == :scissors && self.play == :rock
+      true
+    elsif self.computer_play == :rock && self.play == :paper
+      true 
+    elsif self.computer_play == :paper && self.play == :scissors
       true  
     else
       false
@@ -34,7 +45,7 @@ class RPSGame
   end
 
   def tied?
-    if @computer_play == @play 
+    if self.computer_play == self.play 
       true
     else
       false
@@ -42,7 +53,7 @@ class RPSGame
   end
 
   def lost?
-    if won?
+    if won? 
       false
     elsif tied?
       false
@@ -60,6 +71,5 @@ class RPSGame
       "Human lost!"
     end
   end
-
 
 end
